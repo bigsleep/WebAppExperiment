6b136781398957b17e3e251a5218df24b4114dd5
mt <mt@T420s.(none)>
2014-02-16 05:20:53 +0900

    テスト追加。むき出しのintはdecodeに失敗する。JSONじゃない?

--- whone/test/Spec.hs
+++ whone/test/Spec.hs
@@ -1,48 +1,92 @@
-{-# LANGUAGE TypeOperators, FlexibleInstances #-}
+{-# LANGUAGE TypeOperators, FlexibleInstances, ScopedTypeVariables, OverloadedStrings #-}
 module Main where
 
-import Whone.Internal ((:+:)(..), (:<:), App)
+import Whone.Internal ((:+:)(..), (:<:), App(..))
 import Whone.JsonApi (JsonApi(..), getRequest, putResponse)
 import Whone.Error (IError(..))
 
 import Control.Monad (liftM)
 import Control.Monad.Trans (lift)
-import Control.Monad.Error (catchError)
-import Control.Monad.Reader (ReaderT(..), ask)
+import Control.Monad.State (put)
+import Control.Monad.Reader (ask)
+import Control.Monad.Error (catchError, throwError)
+import Control.Monad.RWS (RWS(..), runRWS)
 import Control.Monad.Free (Free(..))
-import qualified Data.Aeson as DA (FromJSON, ToJSON, decode)
+import qualified Data.Aeson as DA (FromJSON, ToJSON, decode, encode)
 import qualified Data.ByteString.Lazy as L (ByteString, readFile)
 
-data S i o = S
-type F = JsonApi :+: IError String
-type WebApp = App F
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import qualified Test.QuickCheck.Property as P
 
 main :: IO ()
-main = putStrLn "test"
+main = hspec $ jsonApiSpec >> throwErrorApiSpec
 
-wapp :: (DA.FromJSON i, DA.ToJSON o) => (i -> WebApp o) -> WebApp ()
+-- テスト
+jsonApiSpec :: Spec
+jsonApiSpec = prop "入力をそのまま返すAPI" $
+    \(i :: [Int]) ->
+        let s = DA.encode i
+            (_, r, _) = runRWS (run' jsonApi1) s ""
+            message = "result: " ++ show r ++ " expected: " ++ show s
+            result = if r == s
+                        then P.succeeded
+                        else P.failed {P.reason = message}
+        in result
+
+throwErrorApiSpec :: Spec
+throwErrorApiSpec = prop "入力が空の場合にエラーになるテスト" $
+    \(i :: [Int]) ->
+        let s = DA.encode i
+            (_, r, _) = runRWS (run' maybeThrowErrorApi1) s ""
+            expected = if i == [] then DA.encode ("error: input should be greater 0" :: String) else s
+            message = "result: " ++ show r ++ " expected: " ++ show expected
+            result = if r == expected
+                        then P.succeeded
+                        else P.failed {P.reason = message}
+        in result
+
+-- テスト用API
+type F = JsonApi :+: IError String
+type JsonApp = App F
+
+wapp :: (DA.FromJSON i, DA.ToJSON o) => (i -> JsonApp o) -> JsonApp ()
 wapp doSomeThing = (getRequest >>= doSomeThing >>= putResponse)
                    `catchError` putErrorResponse
 
-putErrorResponse :: String -> WebApp ()
+putErrorResponse :: String -> JsonApp ()
 putErrorResponse = putResponse
 
-type Mock = ReaderT L.ByteString Maybe
+jsonApi1 :: JsonApp ()
+jsonApi1 = wapp (return . (id :: [Int] -> [Int]))
+
+maybeThrowErrorApi1 :: JsonApp ()
+maybeThrowErrorApi1 = wapp f
+    where f :: [Int] -> JsonApp [Int]
+          f i = if i == [] then throwError ("error: input should be greater 0" :: String)
+                           else return i
+
+-- プログラム実行用関数
+type Mock = RWS L.ByteString () L.ByteString ()
+
+run' :: JsonApp a -> Mock
+run' (App (Free a)) = run a
+run' (App (Pure a)) = return ()
 
 class (Functor f) => Run f where
-    run :: f (Free F a) -> Mock a
+    run :: f (Free F a) -> Mock
 
 instance (Run f, Run g) => Run (f :+: g) where
     run (Inl a) = run a
     run (Inr a) = run a
 
-run' :: Free F a -> Mock a
-run' (Free a) = run a
-run' (Pure a) = return a
-
 instance Run JsonApi where
-    run (GetRequest f) = ask >>= lift . DA.decode >>= run' . f
-    run (PutResponse _ c) = run' c
+    run (GetRequest f) = ask >>= \s ->
+                         case DA.decode s of
+                              Just i -> run' . App . f $ i
+                              Nothing -> run (ThrowError ("parse error: " ++ show s :: String))
+    run (PutResponse o c) = put (DA.encode o) >> run' (App c)
 
 instance Run (IError String) where
-    run (ThrowError _) = lift Nothing
+    run (ThrowError s) = put (DA.encode s) >> return ()
+
