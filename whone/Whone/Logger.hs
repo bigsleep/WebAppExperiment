7aebdf75421c25046a082abf8dc61c7b14ae3e88
mt <mt@T420s.(none)>
2014-02-19 00:44:22 +0900

    Logger追加

---
+++ whone/Whone/Logger.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TypeOperators, FlexibleContexts, FlexibleInstances, ExistentialQuantification, TypeFamilies #-}
+module Whone.ILogger
+( ILogger(..)
+, Logger
+, LogLevel
+, getCurrentLogLevel
+, log
+, logDebug
+, logInfo
+, logWarning
+, logError
+) where
+
+import Whone.Internal
+import Prelude hiding (log)
+import Control.Monad (when)
+import Control.Monad.Trans.Free (FreeT(..), FreeF(..))
+
+data LogLevel = DEBUG | INFO | WARNING | ERROR deriving (Show, Read, Eq, Ord, Enum)
+
+class Logger a where
+    type OutputType a
+
+data ILogger logger a =
+    GetCurrentLogLevel logger (LogLevel -> a) |
+    Log logger LogLevel (OutputType logger) a
+
+instance Functor (ILogger logger) where
+    fmap f (GetCurrentLogLevel l g) = GetCurrentLogLevel l (f . g)
+    fmap f (Log lg lv s a) = Log lg lv s (f a)
+
+getCurrentLogLevel :: (Functor f, Monad m, ILogger logger :<: f) => logger -> App f m LogLevel
+getCurrentLogLevel logger = App . inject $ GetCurrentLogLevel logger $ FreeT . return . Pure
+
+log :: forall m logger f. (Monad m, Logger logger, ILogger logger :<: f) => LogLevel -> logger -> OutputType logger -> App f m ()
+log level logger contents = do
+    clevel <- getCurrentLogLevel logger
+    when (level >= clevel) (App . inject $ Log logger level contents (FreeT . return . Pure $ ()))
+
+logDebug, logInfo, logWarning, logError :: (Monad m, Logger logger, ILogger logger :<: f) => logger -> OutputType logger -> App f m ()
+logDebug = log DEBUG
+logInfo = log INFO
+logWarning = log WARNING
+logError = log ERROR
