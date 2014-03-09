9013a6314cf916005b7a4fb99fe5c894d8595925
mt <mt@T420s.(none)>
2014-03-09 16:03:42 +0900

    エラーレスポンス

--- whone/Whone/JsonApi.hs
+++ whone/Whone/JsonApi.hs
@@ -2,11 +2,17 @@
 module Whone.JsonApi
 ( JsonApi(..)
 , jsonApi
+, HttpError(..)
 ) where
 
 import Whone.Internal
+
+import Control.Monad.Error (Error, noMsg, strMsg)
 import Control.Monad.Trans.Free (liftF)
+import qualified Data.ByteString.Lazy as L (ByteString, empty)
+import qualified Data.ByteString.Lazy.Char8 as L (pack)
 import qualified Data.Aeson as DA (FromJSON, ToJSON)
+import qualified Network.HTTP.Types as HTTP (Status, Header, status404)
 
 data JsonApi m a = forall i o. (DA.FromJSON i, DA.ToJSON o) =>JsonApi (i -> m o) a
 
@@ -15,3 +21,13 @@ instance Functor (JsonApi m) where
 
 jsonApi :: (DA.FromJSON i, DA.ToJSON o, Monad m, JsonApi n :<: f) => (i -> n o) -> App f m ()
 jsonApi api = App . liftF . inject . JsonApi api $ ()
+
+data HttpError = HttpError {
+    httpErrorStatus :: HTTP.Status,
+    httpErrorHeader :: [HTTP.Header],
+    httpErrorBody :: L.ByteString
+    } deriving (Show, Eq)
+
+instance Error HttpError where
+    noMsg = HttpError HTTP.status404 [] L.empty
+    strMsg = HttpError HTTP.status404 [] . L.pack
--- whone/test/Whone/Backends/JsonApi/WaiTest.hs
+++ whone/test/Whone/Backends/JsonApi/WaiTest.hs
@@ -3,11 +3,12 @@ module Whone.Backends.JsonApi.WaiTest
 ( run
 ) where
 
-import Whone.JsonApi (JsonApi(..))
+import Whone.JsonApi (JsonApi(..), HttpError(..))
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ask)
 import Control.Monad.State (MonadState, put)
+import Control.Monad.Error (MonadError, catchError)
 import qualified Data.Aeson as DA (FromJSON, ToJSON, decode, encode)
 import qualified Data.ByteString.Lazy as L (fromChunks)
 
@@ -17,7 +18,7 @@ import qualified Network.Wai.Test as WT (runSession, request, SResponse)
 import qualified Data.Conduit as C (($$))
 import qualified Data.Conduit.List as CL (consume)
 
-run :: (Monad m, MonadIO m, MonadReader W.Request m, MonadState WT.SResponse m)
+run :: (Monad m, MonadIO m, MonadReader W.Request m, MonadState WT.SResponse m, MonadError HttpError m)
     => (forall a. n a -> m a) -> (forall b. m b -> IO b) -> JsonApi n (m c) -> m c
 run toM toIO (JsonApi app c) = do
     req <- ask
@@ -25,14 +26,15 @@ run toM toIO (JsonApi app c) = do
     put res
     c
 
-toApplication :: (DA.FromJSON i, DA.ToJSON o, Monad m, MonadIO m)
+toApplication :: (DA.FromJSON i, DA.ToJSON o, Monad m, MonadIO m, MonadError HttpError m)
               => (forall b. m b -> IO b) -> (i -> m o) -> W.Application
 toApplication toIO app = \request -> W.requestBody request C.$$ sink
     where sink = do
                  i <- CL.consume
                  case DA.decode . L.fromChunks $ i of
-                      Just input -> putResponse $ app input
+                      Just input -> liftIO . toIO $ (app input >>= putResponse) `catchError` putErrorResponse
                       Nothing -> badRequest
-          putResponse = liftIO . toIO . (return . W.responseLBS status200 [] . DA.encode =<<)
+          putResponse = return . W.responseLBS status200 [] . DA.encode
+          putErrorResponse (HttpError s h b) = return $ W.responseLBS s h b
           badRequest = return $ W.responseLBS status400 [] "error"
 
